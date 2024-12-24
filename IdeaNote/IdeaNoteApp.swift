9362780a4074573d94d8c8d9a0ca862db21f47c6
JohnJin007 <2219566118@qq.com>
2024-12-24 16:18:06 +0800

    Initial Commit

---
+++ IdeaNote/IdeaNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdeaNoteApp.swift
+//  IdeaNote
+//
+//  Created by v_jinlilili on 24/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct IdeaNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
